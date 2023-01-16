b789774fde1b68cc08d42994935264af38be3052
JMarques98 <juliomarqueseman98@gmail.com>
2023-01-16 11:33:52 +0100

    script

---
+++ scripts/installchaincode.sh
@@ -0,0 +1,100 @@
+#!/bin/sh
+
+
+export CORE_PEER_TLS_ENABLED=true
+export CORE_PEER_LOCALMSPID="Org1MSP"
+export CORE_PEER_TLS_ROOTCERT_FILE=${PWD}/organizations/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt
+export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp
+export CORE_PEER_ADDRESS=localhost:7051
+export FABRIC_CFG_PATH=$PWD/../config/
+export PATH=${PWD}/../bin:$PATH
+
+echo "You are on ORG1!"; 
+echo " Lets delete the chaincode package if it exists...."
+
+rm basic.tar.gz
+
+echo "let's pack the chaincode again ...."
+peer version
+peer lifecycle chaincode package basic.tar.gz --path ../lfd272/chaincodes/simple_chaincode --lang node --label basic_1.0
+
+echo "Now the chaincode is packaged and ready to be installed in organizations.!"; 
+
+sleep 5
+
+echo "Lets go to install in all Orgs!"; 
+
+export CORE_PEER_TLS_ENABLED=true
+export CORE_PEER_LOCALMSPID="Org1MSP"
+export CORE_PEER_TLS_ROOTCERT_FILE=${PWD}/organizations/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt
+export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp
+export CORE_PEER_ADDRESS=localhost:7051
+echo "Installing... Be patient!"; 
+
+peer lifecycle chaincode install basic.tar.gz
+
+echo "Chaincode install in Org1!"; 
+
+sleep 3
+
+export CORE_PEER_TLS_ENABLED=true
+export CORE_PEER_LOCALMSPID="Org2MSP"
+export CORE_PEER_TLS_ROOTCERT_FILE=${PWD}/organizations/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/ca.crt
+export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org2.example.com/users/Admin@org2.example.com/msp
+export CORE_PEER_ADDRESS=localhost:9051
+echo "Installing... Be patient!"; 
+peer lifecycle chaincode install basic.tar.gz
+
+echo "Chaincode install in Org2!"; 
+
+peer lifecycle chaincode queryinstalled
+
+export CC_PACKAGE_ID=basic_1.0:b4082e8d4bc509e8f40f436164ba6cca38bab15e7612d2fda5b887fbe120a8d3
+
+echo "Orgs must approve the chaincode"; 
+echo "lets go...!"
+
+export CORE_PEER_TLS_ENABLED=true
+export CORE_PEER_LOCALMSPID="Org1MSP"
+export CORE_PEER_TLS_ROOTCERT_FILE=${PWD}/organizations/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt
+export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp
+export CORE_PEER_ADDRESS=localhost:7051
+echo "Installing... Be patient!"; 
+peer lifecycle chaincode approveformyorg -o localhost:7050 --ordererTLSHostnameOverride orderer.example.com --channelID mychannel --name basic --version 1.0 --package-id $CC_PACKAGE_ID --sequence 1 --tls --cafile ${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem
+echo "Chaincode approve in Org1!"; 
+echo "Lets go with the Org2!"; 
+
+sleep 3
+
+export CORE_PEER_TLS_ENABLED=true
+export CORE_PEER_LOCALMSPID="Org2MSP"
+export CORE_PEER_TLS_ROOTCERT_FILE=${PWD}/organizations/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/ca.crt
+export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org2.example.com/users/Admin@org2.example.com/msp
+export CORE_PEER_ADDRESS=localhost:9051
+
+echo "Installing... Be patient!"; 
+
+peer lifecycle chaincode approveformyorg -o localhost:7050 --ordererTLSHostnameOverride orderer.example.com --channelID mychannel --name basic --version 1.0 --package-id $CC_PACKAGE_ID --sequence 1 --tls --cafile ${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem
+
+echo "Chaincode approve in Org2!"; 
+
+echo "Lets see all our Orgs which have the chaincode"
+peer lifecycle chaincode checkcommitreadiness --channelID mychannel --name basic --version 1.0 --sequence 1 --tls --cafile ${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem --output json
+
+sleep 3
+
+echo "Lets commit the chaincode into the channel with both organizations.... "
+
+peer lifecycle chaincode commit -o localhost:7050 --ordererTLSHostnameOverride orderer.example.com --channelID mychannel --name basic --version 1.0 --sequence 1 --tls --cafile ${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem --peerAddresses localhost:7051 --tlsRootCertFiles ${PWD}/organizations/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt --peerAddresses localhost:9051 --tlsRootCertFiles ${PWD}/organizations/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/ca.crt
+
+echo "Commiteeeed!! "
+
+sleep 3
+
+echo "Now we want to see what we have installed...!"
+
+peer lifecycle chaincode querycommitted --channelID mychannel --name basic --cafile ${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.solar.com-cert.pem
+
+
+echo "Okey! You have the basic chaincode installed in your organizations! Now you can play with the functions implemented!"
+
